72583f9ea4a5b85aa71b543590436d76e189b935
Ankan <ankanganguly24@gmail.com>
2024-08-11 14:05:36 +0530

    GAllery seperated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
-"use client"
+"use client";
 import { Assosciations } from "@/components/ui/Association";
+import Gallery from "@/components/ui/Gallery";
 import AboutUniversity from "@/components/ui/about-uni";
 import CampusInfo from "@/components/ui/campus-info";
 import GoogleReviews from "@/components/ui/google-reviews";
@@ -7,25 +8,26 @@ import Header from "@/components/ui/header";
 import Location from "@/components/ui/location";
 import TestimonialCard from "@/components/ui/testimonial-card";
 import Whyus from "@/components/ui/why-us";
-import Gallery from "./gallery/page";
 
-const Home = () =>  {
+const Home = () => {
   return (
-  <div>
-   <Header />
-   <AboutUniversity />
-   <Whyus />
-   <CampusInfo />
-   <GoogleReviews />
-   <TestimonialCard />
-   <Assosciations />
-   <div>
-      <h1 className="text-4xl font-bold text-primary text-center my-8">Gallery</h1>
-   <Gallery count={8} showHeader={false} />
+    <div>
+      <Header />
+      <AboutUniversity />
+      <Whyus />
+      <CampusInfo />
+      <GoogleReviews />
+      <TestimonialCard />
+      <Assosciations />
+      <div>
+        <h1 className="text-4xl font-bold text-primary text-center my-8">
+          Gallery
+        </h1>
+        <Gallery count={8} showHeader={false} />
+      </div>
+      <Location />
     </div>
-   <Location />
-  </div>
   );
-}
+};
 
 export default Home;
