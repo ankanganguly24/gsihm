597b52890d4a6a5d04e9da0722044477c39a22ce
Ankan <ankanganguly24@gmail.com>
2025-06-28 00:28:49 +0530

    Update Location component to change embedded Google Maps iframe source for accurate location display

--- src/components/ui/location.tsx
+++ src/components/ui/location.tsx
@@ -8,7 +8,8 @@ const Location = () => {
         </div>
 
         <div className='w-[100%]'>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3563.5621664431287!2d88.41355227513883!3d26.72643096807171!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39e441fdd1c11c7f%3A0xd05eeffa24dfe48a!2sGlobal%20Spectra%20Institute!5e0!3m2!1sen!2sin!4v1720854361011!5m2!1sen!2sin" className='w-full h-80' loading="lazy" ></iframe>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7124.615145828867!2d88.378901!3d26.766464!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39e4476b0ea850f5%3A0xc26811584f7e86ef!2sGodwin%20Modern%20School!5e0!3m2!1sen!2sus!4v1751050638991!5m2!1sen!2sus"  loading="lazy" className='w-full h-80'></iframe>
+      
         </div>
     </div>
   )
